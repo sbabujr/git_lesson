65db217c7613b8628776d970ac318e18c8ad398f
sthavana <sthavana@cisco.com>
2019-05-01 20:19:55 +0530

    changed to y and remove x

--- src/my_square.py
+++ src/my_square.py
@@ -1,15 +1,11 @@
 def my_square(y):
 	"""takes a value and returns the squared value.
 	
-	uses the ** operator
-	this file changes in github again
+	uses the * operator
+	
 	"""
-	return(y ** 2)
+	return(y * y)
 
-def my_square2(x):
-	"""uses the * operator to calculate square
-	"""
-	return(x * x)
 
 print(my_square(3))
-print(my_square2(3))
+
