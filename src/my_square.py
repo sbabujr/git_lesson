69dfc9f7a17a7397c9abe5f6bf46d158eeb8bb5a
sthavana <sthavana@cisco.com>
2019-05-01 19:50:59 +0530

    python script to square the given value

---
+++ src/my_square.py
@@ -0,0 +1,14 @@
+def my_square(y):
+	"""takes a value and returns the squared value.
+	
+	uses the ** operator
+	"""
+	return(y ** 2)
+
+def my_square2(x):
+	"""uses the * operator to calculate square
+	"""
+	return(x * x)
+
+print(my_square(42))
+print(my_square2(42))
